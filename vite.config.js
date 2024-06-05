f799fdbf07cb761de7b226649d90086555563a3c
Sam Jones <samjones@Sams-MacBook-Pro.local>
2024-06-05 21:25:16 +0100

    put vite config back

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
 	build: {
 		lib: {
@@ -18,7 +19,5 @@ export default defineConfig({
 			}
 		}
 	},
-  	plugins: [
-		vue()
-	]
+  	plugins: [vue()]
 });
